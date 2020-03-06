e67088bd089ccb35c7f2b25fae3c4f4e1f5a7780
Charles <charles.sirois14@gmail.com>
2020-03-06 13:14:40 -0500

    PID implemantation, not tested

--- Self_balanced_bike/include/Motor_Controller.h
+++ Self_balanced_bike/include/Motor_Controller.h
@@ -2,6 +2,7 @@
 #define MOTOR_CONTROLLER_H
 
 #include "Encoder.h"
+#include <PID_v1.h>
 
 //Define
 #define CW true
@@ -13,7 +14,7 @@
 #define MOTOR_PIN_1 1
 #define MOTOR_PIN_2 2
 
-#define COUNT_TO_ANGLE 360/(21*11)
+#define COUNT_TO_ANGLE 360/(21.3*48)
 #define SPEED_INTERVAL 300000 //Interval to measure speed (in uS)
 #define USEC_TO_SEC 1000000
 
@@ -26,18 +27,27 @@ class FlywheelMotor{
         double _prevAngle;
         double _currentAngle;
 
+        //PID variables
+        PID _speedPID;
+        double _targetSpeed;
+        double _speedCommand;
+        double _Kp, _Kd, _Ki;
+
+        //Pinout
         int _pin1 = MOTOR_PIN_1;
         int _pin2 = MOTOR_PIN_2;
         
     public:
-        double readAngle();
         FlywheelMotor();
+
+        double readAngle();
         void measureSpeed();
 
         double getSpeed();
         double getAngle();
 
         void setMotorSpeed(int speed, bool dir);
+        void setMotorSpeedPID(double targetSpeed);
         void brakeMotor();
 };
 
--- Self_balanced_bike/src/Motor_Controller.cpp
+++ Self_balanced_bike/src/Motor_Controller.cpp
@@ -6,28 +6,43 @@ static void measureSpeedTimer(){
 
 FlywheelMotor flywheelMotor;
 
-FlywheelMotor::FlywheelMotor():_motor_enc(ENC_PIN_1, ENC_PIN_2), _speedTimer(){
+//Constructeur
+FlywheelMotor::FlywheelMotor(): _motor_enc(ENC_PIN_1, ENC_PIN_2), 
+                                _speedTimer(), 
+                                _speedPID(&_speed, &_speedCommand, &_targetSpeed, _Kp, _Kd, _Ki, DIRECT){
     _prevAngle = 0;
     _currentAngle = 0;
 
     _speedTimer.begin(measureSpeedTimer, 50);
 
+    _Kp = 4;
+    _Kd = 0;
+    _Ki = 1;
+
+    _speedPID.SetOutputLimits(0, 1023);
+    _speedPID.SetTunings(_Kp, _Kd, _Ki);
+
+
     pinMode(_pin1, OUTPUT);
     pinMode(_pin2, OUTPUT);
 }
 
+//Return current motor angle
 double FlywheelMotor::getAngle(){
     return _currentAngle;
 }
 
+//Return current motor speed
 double FlywheelMotor::getSpeed(){
     return _speed;
 }
 
+//Return encoder value
 double FlywheelMotor::readAngle(){
     return _motor_enc.read()*COUNT_TO_ANGLE;
 }
 
+//Calculate motor speed
 void FlywheelMotor::measureSpeed(){
     _prevAngle = _currentAngle;
     _currentAngle = readAngle();
@@ -35,6 +50,7 @@ void FlywheelMotor::measureSpeed(){
     _speed = (_currentAngle - _prevAngle)/SPEED_INTERVAL*USEC_TO_SEC;
 }
 
+//Set speed of the motor, dir:CW or CCW
 void FlywheelMotor::setMotorSpeed(int speed, bool dir){
     if(dir == CW){
         analogWrite(_pin1, 0);
@@ -46,6 +62,24 @@ void FlywheelMotor::setMotorSpeed(int speed, bool dir){
     }
 }
 
+//Set speed of the flywheel with PID, if targetSpeed < 0 => CCW
+void FlywheelMotor::setMotorSpeedPID(double targetSpeed){
+    _targetSpeed = targetSpeed;
+    bool dir = _targetSpeed < 0 ? CCW : CW;
+
+    _speedPID.Compute();
+
+    if(dir == CW){
+        analogWrite(_pin1, 0);
+        analogWrite(_pin2, _speedCommand);
+    }
+    else{
+        analogWrite(_pin1, _speedCommand);
+        analogWrite(_pin2, 0);
+    }
+}
+
+//Brake motor
 void FlywheelMotor::brakeMotor(){
     digitalWrite(_pin1, HIGH);
     digitalWrite(_pin2, HIGH);
